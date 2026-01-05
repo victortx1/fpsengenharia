ea213506c6ae37fc80b85ec6b37a4259e8a73e39
Victor <delarassistente@mgail.com>
2026-01-05 16:28:48 -0300

    Atualização do site

---
+++ script.js
@@ -0,0 +1,15 @@
+const elements = document.querySelectorAll('[data-animate]');
+
+function animateOnScroll() {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', animateOnScroll);
+animateOnScroll();
